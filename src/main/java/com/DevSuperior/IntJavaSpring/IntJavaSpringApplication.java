7e1babe1d5fd318be80d39a876bad37dcd91b140
Hugo Carvalho <hugocarvmelo@gmail.com>
2023-07-12 20:09:31 -0300

    Criação do Projeto

---
+++ src/main/java/com/DevSuperior/IntJavaSpring/IntJavaSpringApplication.java
@@ -0,0 +1,13 @@
+package com.DevSuperior.IntJavaSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntJavaSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntJavaSpringApplication.class, args);
+	}
+
+}
